2a4d728e84e83ac02e7ffb31590095ee82d3e156
naman394 <150386505+naman394@users.noreply.github.com>
2024-08-08 14:40:13 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const getTodosSync = () => {};
+
+const getTodoSync = (id) => {};
+
+const createTodoSync = (todo) => {};
+
+const updateTodoSync = (id, updates) => {};
+
+const deleteTodoSync = (id) => {};
+
+const getTodos = () => {};
+
+const getTodo = (id) => {};
+
+const createTodo = (todo) => {};
+
+const updateTodo = async (id, updates) => {};
+
+const deleteTodo = async (id) => {};
+
+module.exports = {
+  getTodosSync,
+  getTodoSync,
+  createTodoSync,
+  updateTodoSync,
+  deleteTodoSync,
+  getTodos,
+  getTodo,
+  createTodo,
+  deleteTodo,
+  updateTodo,
+};
