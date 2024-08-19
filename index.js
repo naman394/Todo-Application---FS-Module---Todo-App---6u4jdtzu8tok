0231d5140da1809160451c23fba66a40fcb804be
naman394 <navnitnaman48@gmail.com>
2024-08-19 23:56:01 +0530

    a

--- index.js
+++ index.js
@@ -1,22 +1,49 @@
-const getTodosSync = () => {};
-
-const getTodoSync = (id) => {};
-
-const createTodoSync = (todo) => {};
-
-const updateTodoSync = (id, updates) => {};
-
-const deleteTodoSync = (id) => {};
-
-const getTodos = () => {};
-
-const getTodo = (id) => {};
-
-const createTodo = (todo) => {};
-
-const updateTodo = async (id, updates) => {};
-
-const deleteTodo = async (id) => {};
+const {appendFileSync: AFS, readFileSync: RFS, writeFileSync:WFS} = require('fs'); 
+const getTodosSync = () => {   return RFS('./db.txt', 'utf8') };
+const convertStringtoJson = (item) => {   
+  return JSON.parse(`[${item.split('\n}{').join('},{')}]`); 
+} 
+const getTodoSync = (id) => {   
+  let k = convertStringtoJson(getTodosSync())   
+  for (let i of k){     
+    if ((i.id) == id){       
+      return (JSON.stringify(i))     
+    }   
+  } 
+};  
+const createTodoSync = (todo) => {   
+  let obj = {     
+    "id" :  Date.now(),     
+    "title" : todo,     
+    "isCompleted": false,     
+    "createdAt": new Date().toISOString(),     
+    "updatedAt": new Date().toISOString()   
+  }   
+  AFS('./db.txt', JSON.stringify(obj,null,2)) 
+}; 
+const updateTodoSync = (id, updates) => {   
+  let todos = convertStringtoJson(getTodosSync());   
+  const index = todos.findIndex(todo => todo.id == id);   
+  let s = "";   
+  if (index !== -1) {     
+    todos[index] = {...todos[index],...updates,updatedAt: new Date().toISOString()};     
+    for (let i of todos){       
+      s += JSON.stringify(i, null, 2);     
+    }     
+    WFS('./db.txt', s, 'utf8');   
+  }; 
+}  
+const deleteTodoSync = (id) => {   
+  let todos = convertStringtoJson(getTodosSync());      
+  const initialLength = todos.length;   todos = todos.filter(todo => todo.id != id);   
+  let s = "";   
+  if (todos.length < initialLength) {     
+    for (let i of todos){       
+      s += JSON.stringify(i, null, 2);     
+    }     
+    WFS('./db.txt', s, 'utf8');   
+  } 
+};
 
 module.exports = {
   getTodosSync,
@@ -24,9 +51,4 @@ module.exports = {
   createTodoSync,
   updateTodoSync,
   deleteTodoSync,
-  getTodos,
-  getTodo,
-  createTodo,
-  deleteTodo,
-  updateTodo,
 };
